0118391b04e1b12c29d796b226a0b543859d2b32
j4f-dev <dangerog89@gmail.com>
2025-12-28 07:08:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,46 @@
-"""
-j4f-cli-toolkit
-Author: Sohail
-Description: A clean and professional Python CLI toolkit.
-"""
-
+from toolkit.system import system_info
+from toolkit.network import network_info
+from toolkit.utils import clear_screen
 import sys
-from toolkit.system import system_info, clear_screen
 
-
-def show_banner():
+def banner():
     clear_screen()
     print("""
-====================================
-        j4f-cli-toolkit
-====================================
-A Professional Python CLI Toolkit
-""")
+    ██████╗ ██╗  ██╗███████╗
+    ██╔══██╗██║  ██║██╔════╝
+    ██████╔╝███████║█████╗
+    ██╔═══╝ ██╔══██║██╔══╝
+    ██║     ██║  ██║███████╗
+    ╚═╝     ╚═╝  ╚═╝╚══════╝
 
+        J4F CLI TOOLKIT
+    """)
 
-def show_help():
-    print("""
-Available Commands
-------------------
-info    Show system information
-clear   Clear the screen
-help    Show this help menu
-exit    Exit the toolkit
-""")
-
-
-def main():
-    show_banner()
-
+def menu():
     while True:
-        command = input("j4f > ").strip().lower()
+        print("""
+[1] System Information
+[2] Network Information
+[3] Clear Screen
+[4] Toolkit Info
+[5] Exit
+""")
+        choice = input("j4f > ")
 
-        if command == "info":
+        if choice == "1":
             system_info()
-        elif command == "clear":
-            show_banner()
-        elif command == "help":
-            show_help()
-        elif command == "exit":
-            print("Exiting j4f-cli-toolkit...")
-            sys.exit(0)
+        elif choice == "2":
+            network_info()
+        elif choice == "3":
+            banner()
+        elif choice == "4":
+            print("\nJ4F CLI Toolkit\nBuilt for learning & automation")
+        elif choice == "5":
+            print("👋 Exiting J4F CLI Toolkit")
+            sys.exit()
         else:
-            print("Unknown command. Type 'help' to see available commands.")
-
+            print("❌ Invalid option")
 
 if __name__ == "__main__":
-    main()
+    banner()
+    menu()
