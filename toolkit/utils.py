ecb0739285601518264ba9041ee4da9aae5b27f6
j4f-dev <dangerog89@gmail.com>
2025-12-28 07:24:32 +0530

    Update utils.py

--- toolkit/utils.py
+++ toolkit/utils.py
@@ -1,16 +1,21 @@
 import os
+import sys
+import time
+
+class Color:
+    GREEN = "\033[92m"
+    CYAN = "\033[96m"
+    RED = "\033[91m"
+    YELLOW = "\033[93m"
+    RESET = "\033[0m"
+    BOLD = "\033[1m"
 
 def clear_screen():
-    os.system("clear" if os.name != "nt" else "cls")
+    os.system("clear")
 
-def banner():
-    clear_screen()
-    print("""
-███████╗██╗  ██╗███████╗
-██╔════╝██║  ██║██╔════╝
-█████╗  ███████║█████╗
-██╔══╝  ██╔══██║██╔══╝
-███████╗██║  ██║███████╗
-
-j4f-cli-toolkit
-""")
+def slow_print(text, delay=0.02):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
